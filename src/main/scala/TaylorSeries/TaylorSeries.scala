efc835fb61c0c98211c32cac484cc350d4bcfe5b
kevin mc inerney <kevinmcinerney8@gmail.com>
2016-11-27 21:19:38 +0000

    added toDecPlaces functionality

--- src/main/scala/TaylorSeries/TaylorSeries.scala
+++ src/main/scala/TaylorSeries/TaylorSeries.scala
@@ -3,33 +3,53 @@ package TaylorSeries
 /**
   * Created by kevin on 26/11/16.
   */
+import Math._
 
-class TaylorSeries {
+abstract class TaylorFunction(val base: Int)
 
-  abstract class TaylorFunction
+case class Sin(x: Double) extends TaylorFunction(1)
+case class Cos(x: Double) extends TaylorFunction(0)
+case class E(x: Double) extends TaylorFunction(0)
 
-  case class Sin(x: Double) extends TaylorFunction
-  case class Cos(x: Double) extends TaylorFunction
-  case class E(x: Double) extends TaylorFunction
+object TaylorSeries {
+
+  def calculate(t: TaylorFunction, nDecPlaces: Int, degrees: Int): Double = {
+
+    if(degrees - 2 < 0){
+      val cur = calculateHelper(t, degrees + 2)
+      val prev = calculateHelper(t, degrees)
+      if (accurateEnough(cur, prev, nDecPlaces)) cur else calculate(t, nDecPlaces, degrees + 2)
+    }else{
+      val cur = calculateHelper(t, degrees)
+      val prev = calculateHelper(t, degrees - 2)
+      if (accurateEnough(cur, prev, nDecPlaces)) cur else calculate(t, nDecPlaces, degrees + 2)
+    }
 
 
-  def calculate(f: TaylorFunction, degree: Int): Double = f match {
-    case Sin(x: Double) => sin(x, 1, degree, true)
-    case Cos(x: Double) => 0f
+  }
+
+ private def calculateHelper(t: TaylorFunction, degree: Int) : Double = t match {
+    case Sin(x: Double) => sin(x, t.base, degree, positive = true)
+    case Cos(x: Double) => cos(x, t.base, degree, positive = true)
     case E(x: Double) => 0f
   }
 
+
   def sin(x: Double, n: Int, d: Int, positive: Boolean): Double = positive match {
 
-    case false if n == d => (1.0 / factorial(n)) * Math.pow(x, n)
-    case true  if n == d => (1.0 / factorial(n)) * Math.pow(x, n)
-    case false => (1.0 / factorial(n)) * Math.pow(x, n) + sin(x, n + 2, d, positive = true)
-    case true  => (1.0 / factorial(n)) * Math.pow(x, n) - sin(x, n + 2, d, positive = false)
+    case false if n >= d => {println("d = " + n); (1.0 / factorial(n)) * pow(x, n)}
+    case true  if n >= d => {println("d = " + n); (1.0 / factorial(n)) * pow(x, n)}
+    case false => {println("d = " + n); (1.0 / factorial(n)) * pow(x, n) + sin(x, n + 2, d, positive = true)}
+    case true  => {println("d = " + n); (1.0 / factorial(n)) * pow(x, n) - sin(x, n + 2, d, positive = false)}
 
   }
 
-  def cos(x: Int, degree: Int): Float = {
-    ???
+  def cos(x: Double, n: Int, d: Int, positive: Boolean): Double = positive match {
+
+    case false if n >= d => (1.0 / factorial(n)) * pow(x, n)
+    case true  if n >= d => (1.0 / factorial(n)) * pow(x, n)
+    case false => (1.0 / factorial(n)) * pow(x, n) + cos(x, n + 2, d, positive = true)
+    case true  => (1.0 / factorial(n)) * pow(x, n) - cos(x, n + 2, d, positive = false)
   }
 
   def e(x: Int, degree: Int): Float = {
@@ -41,15 +61,28 @@ class TaylorSeries {
     case _ => x * factorial(x-1)
   }
 
+  def accurateEnough(curValue: Double, prevValue: Double, nDecPts: Int): Boolean = {
+    val tmp = pow(10, nDecPts)
+    val prev = round(prevValue * tmp) / tmp
+    val cur = round(curValue * tmp) / tmp
+    println("The difference between f(n): " + cur + " and g(n): " + prev + " = " +  abs(cur - prev))
+    abs(cur - prev) == 0.0
+
+  }
+
 }
 
 object TaylorSeriesRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val ts = new TaylorSeries()
+    val ts = TaylorSeries
 
-    println(" = " + ts.calculate(ts.Sin(0.9), 15))
+    println(" = " + ts.calculate(Sin(0.75), 10, 1))
+
+    println(ts.accurateEnough(0.1233, 0.1234, 4))
+
+
   }
 }
 
--- src/test/scala/TaylorSeriesSuite.scala
+++ src/test/scala/TaylorSeriesSuite.scala
@@ -3,24 +3,36 @@
   */
 
 
-import org.scalatest.FunSuite
+import TaylorSeries._
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
-object TS extends TaylorSeries.TaylorSeries
-import TS._
 
 @RunWith(classOf[JUnitRunner])
 class TaylorSeriesSuite extends FunSuite {
 
+  val ts = TaylorSeries
+  import ts._
+
   test("Sin values are correct") {
 
     val eta = 0.001
-    assert(calculate(Sin(0.2), 9) - Math.sin(0.2) < eta)
-    assert(calculate(Sin(0.3), 9) - Math.sin(0.3) < eta)
-    assert(calculate(Sin(0.4), 9) - Math.sin(0.4) < eta)
-    assert(calculate(Sin(0.5), 9) - Math.sin(0.5) < eta)
-    assert(calculate(Sin(0.7), 9) - Math.sin(0.7) < eta)
+    assert(calculate(Sin(0.2), 3, 1) - Math.sin(0.2) < eta)
+    assert(calculate(Sin(0.3), 3, 1) - Math.sin(0.3) < eta)
+    assert(calculate(Sin(0.4), 3, 1) - Math.sin(0.4) < eta)
+    assert(calculate(Sin(0.5), 3, 1) - Math.sin(0.5) < eta)
+    assert(calculate(Sin(0.7), 3, 1) - Math.sin(0.7) < eta)
+  }
+
+  test("Cos values are correct") {
+
+    val eta = 0.001
+    assert(calculate(Cos(0.2), 3, 1) - Math.cos(0.2) < eta)
+    assert(calculate(Cos(0.3), 3, 1) - Math.cos(0.3) < eta)
+    assert(calculate(Cos(0.4), 3, 1) - Math.cos(0.4) < eta)
+    assert(calculate(Cos(0.5), 3, 1) - Math.cos(0.5) < eta)
+    assert(calculate(Cos(0.7), 3, 1) - Math.cos(0.7) < eta)
   }
 
   test("factorial is correct") {
@@ -29,6 +41,20 @@ class TaylorSeriesSuite extends FunSuite {
     assert(factorial(4) == 24)
     assert(factorial(5) == 120)
     assert(factorial(6) == 720)
+  }
+
+  test("accurateEnough is correct") {
+
+    assert(accurateEnough(0.123, 0.1234, 0))
+    assert(accurateEnough(0.123, 0.1234, 1))
+    assert(accurateEnough(0.123, 0.1234, 2))
+    assert(accurateEnough(0.123, 0.1234, 3))
+    assert(!accurateEnough(0.1233, 0.1234, 4))
+    assert(!accurateEnough(0.12333, 0.12344, 5))
+    assert(!accurateEnough(0.123333, 0.123444, 6))
+
+
+
   }
 
 }
